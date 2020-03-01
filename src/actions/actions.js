4cee52a9f87a3094f1b5ad6bd15878f346b1d1a7
David White <dwhite0627@gmail.com>
2020-03-01 14:28:07 -0500

    Add car API fetching action types

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,5 @@
-import { BUY_ITEM, REMOVE_FEATURE } from "../constants/ActionTypes"
+import { BUY_ITEM, REMOVE_FEATURE, FETCH_CAR_IMAGE_START, FETCH_CAR_IMAGE_SUCCESS, FETCH_CAR_IMAGE_FAIL } from "../constants/ActionTypes"
+import axios from 'axios';
 
 export const buyItem = item => {
   return {
@@ -12,4 +13,16 @@ export const removeFeature = feature => {
     type: REMOVE_FEATURE,
     payload: feature
   }
+}
+
+export const getCarImages = carStr => dispatch => {
+  dispatch({ type: FETCH_CAR_IMAGE_START })
+  axios.get(`https://www.carimagery.com/api.asmx/GetImageUrl?searchTerm={carStr.trim().toLowerCase().split(' ').join('+')}`)
+    .then(res => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(res.data, "text/xml");
+      const src = xml.getElementsByTagName('string')[0].childNodes[0].nodeValue;
+      dispatch({ type: FETCH_CAR_IMAGE_SUCCESS, payload: src })
+    })
+    .catch(err => dispatch({ type: FETCH_CAR_IMAGE_FAIL }))
 }
