4a4b6d16f113e09f8079329972657ab846737788
David White <dwhite0627@gmail.com>
2020-03-01 14:28:33 -0500

    Add react-thunk middleware to the store creation call

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Styles
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
 // Redux imports
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
 // Reducers
-import { carsReducer } from './reducers/CarsReducer';
+import { carsReducer } from "./reducers/CarsReducer";
 
 // Components
-import App from './App';
+import App from "./App";
 
 // Create the redux store
-const store = createStore(carsReducer);
+const store = createStore(carsReducer, applyMiddleware(thunk));
 
 // Mount the App component
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
-    <App /> 
+    <App />
   </Provider>,
   rootElement
 );
