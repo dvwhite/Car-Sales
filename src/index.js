f2af0a3c13c08cb8f1b7054f8d4a6db8aa55a0a5
David White <dwhite0627@gmail.com>
2020-03-01 08:36:01 -0500

    Set up Provider and store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+// Styles
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// Redux imports
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// Reducers
+import { carsReducer } from './reducers/CarsReducer';
+
+// Components
+import App from './App';
+
+// Create the redux store
+const store = createStore(carsReducer);
+
+// Mount the App component
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App /> 
+  </Provider>,
+  rootElement
+);
