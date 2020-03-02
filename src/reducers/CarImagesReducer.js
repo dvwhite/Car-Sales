22c43b2899c54de09806235786b3663bee74369c
David White <dwhite0627@gmail.com>
2020-03-01 21:37:59 -0500

    Create initial state for the carImagesReducer

--- src/reducers/CarImagesReducer.js
+++ src/reducers/CarImagesReducer.js
@@ -3,11 +3,16 @@ import { FETCH_CAR_IMAGE_START, FETCH_CAR_IMAGE_SUCCESS, FETCH_CAR_IMAGE_FAIL } 
 const initialState = {
   isFetching: false,
   error: '',
-  images: []
+  images: [
+    {alt: 'Peugot RCZ', src: 'http://www.regcheck.org.uk/image.aspx/@cGV1Z2VvdCBSQ1o='},
+    {alt: 'Ford Mustang', src: 'http://www.regcheck.org.uk/image.aspx/@Zm9yZCBtdXN0YW5n'},
+    {alt: 'Lamberghini Murcielago', src: 'http://www.regcheck.org.uk/image.aspx/@bGFtYmVyZ2hpbmkgbXVyY2llbGFnbw=='},
+    {alt: 'Porsche 911', src: 'http://www.regcheck.org.uk/image.aspx/@cG9yc2NoZSA5MTE= '},
+    {alt: 'Chevrolet Camaro', src: 'http://www.regcheck.org.uk/image.aspx/@Y2hldnJvbGV0IGNhbWFybw== '}
+  ]
 }
 
 export const carImagesReducer = (state = initialState, action) => {
-  console.log("Redux reducer state:", state)
   switch(action.type) {  
     case FETCH_CAR_IMAGE_START:
       return {
