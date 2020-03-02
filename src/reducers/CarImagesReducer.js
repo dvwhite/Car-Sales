88fa9e58d78ec64b1f58509cb940ef552fe69ce5
David White <dwhite0627@gmail.com>
2020-03-01 22:02:08 -0500

    Update default state

--- src/reducers/CarImagesReducer.js
+++ src/reducers/CarImagesReducer.js
@@ -3,12 +3,12 @@ import { FETCH_CAR_IMAGE_START, FETCH_CAR_IMAGE_SUCCESS, FETCH_CAR_IMAGE_FAIL } 
 const initialState = {
   isFetching: false,
   error: '',
-  images: [
-    {alt: 'Peugot RCZ', src: 'http://www.regcheck.org.uk/image.aspx/@cGV1Z2VvdCBSQ1o='},
-    {alt: 'Ford Mustang', src: 'http://www.regcheck.org.uk/image.aspx/@Zm9yZCBtdXN0YW5n'},
-    {alt: 'Lamberghini Murcielago', src: 'http://www.regcheck.org.uk/image.aspx/@bGFtYmVyZ2hpbmkgbXVyY2llbGFnbw=='},
-    {alt: 'Porsche 911', src: 'http://www.regcheck.org.uk/image.aspx/@cG9yc2NoZSA5MTE= '},
-    {alt: 'Chevrolet Camaro', src: 'http://www.regcheck.org.uk/image.aspx/@Y2hldnJvbGV0IGNhbWFybw== '}
+  cars: [
+    {name: '2015 Peugot RCZ', url: 'http://www.regcheck.org.uk/image.aspx/@cGV1Z2VvdCBSQ1o=', price: 35720},
+    {name: '2020 Ford Mustang', url: 'http://www.regcheck.org.uk/image.aspx/@Zm9yZCBtdXN0YW5n', price: 62440},
+    {name: '2020 Lamberghini Murcielago', url: 'http://www.regcheck.org.uk/image.aspx/@bGFtYmVyZ2hpbmkgbXVyY2llbGFnbw==', price: 210530},
+    {name: '2020 Porsche 911', url: 'http://www.regcheck.org.uk/image.aspx/@cG9yc2NoZSA5MTE= ', price: 309900},
+    {name: '2020 Chevrolet Camaro', url: 'http://www.regcheck.org.uk/image.aspx/@Y2hldnJvbGV0IGNhbWFybw==', price: 86990}
   ]
 }
 
