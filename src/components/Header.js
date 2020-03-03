066cee5f9995eebca934edfa020bc6ef0853a9ab
David White <dwhite0627@gmail.com>
2020-03-02 22:55:38 -0500

    Change car image size in the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Header = props => {
   return (
     <>
-      <figure className="image is-128x128">
+      <figure className="image is-256x256">
         <img src={props.car.image} alt={props.car.name} />
       </figure>
-      <h2>{props.car.name}</h2>
+      <h2>{`${props.car.year} ${props.car.name}`}</h2>
       <p>Amount: ${props.car.price}</p>
     </>
   );
